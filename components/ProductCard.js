dcb0ebb60e1d310465cef4c57feb00d88e1d47b8
Lotte Carleer <lottecarleer@LaptopvanLotte.lan>
2026-03-25 21:31:06 +0100

    feat: added button styling

--- components/ProductCard.js
+++ components/ProductCard.js
@@ -40,6 +40,19 @@ const styles = StyleSheet.create({
     color: "#555",
     marginTop: 4,
   },
+   button: {
+    backgroundColor: "#000000",
+  paddingVertical: 12,
+  paddingHorizontal: 28,
+  borderRadius: 10,
+  marginTop: 10,
+  },
+  buttonText: {
+  color: "#fff",
+  fontSize: 15,
+  fontWeight: "600",
+  textAlign: "center",
+},
 });
 
 export default ProductCard;
